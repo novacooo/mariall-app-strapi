4eee4e86082a5c06c5a82eac63000be40866bd35
Jacek Nowak <jaceknowak226@gmail.com>
2022-10-15 00:38:44 +0200

    Change limit

---
+++ config/plugins.ts
@@ -0,0 +1,8 @@
+export default () => ({
+  graphql: {
+    config: {
+      defaultLimit: 500,
+      maxLimit: 10000,
+    },
+  },
+});
---
+++ dist/config/plugins.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = () => ({
+    graphql: {
+        config: {
+            defaultLimit: 500,
+            maxLimit: 10000,
+        },
+    },
+});
